aab4f670d3bc987f29f80823246831d1c5d8475c
Oliver Willekens <4733178+oliverw1@users.noreply.github.com>
2022-10-18 21:00:15 +0200

    prep for tomorrow

---
+++ tests/test_labellers.py
@@ -0,0 +1,23 @@
+from datetime import date
+
+from pyspark.sql.types import (
+    BooleanType,
+    DateType,
+    StringType,
+    StructType,
+)
+
+from exercises.c_labellers.dates import label_weekend
+from tests.comparers import assert_frames_functionally_equivalent
+
+
+def test_label_weekend(spark):
+    # Tests almost always follow the arrange-act-assert pattern.
+    # ARRANGE
+    ...
+
+    # ACT
+    ...
+
+    # ASSERT
+    assert_frames_functionally_equivalent(result, expected)
